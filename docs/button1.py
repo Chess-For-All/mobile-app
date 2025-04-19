9ef37a76fbf14edbdc73b9377a9b9580617aed77
mihail0011 <mihail.alekseevich.ucheniy@gmail.com>
2025-04-19 13:03:20 +0400

    Create button1.py

---
+++ docs/button1.py
@@ -0,0 +1,11 @@
+x = 0
+
+for i in range(infinity): 
+    def click():
+        if onclick:
+            x += 1;
+            element = document.getElementById('like')
+            element.style.text = 'liked'
+        else:
+            x += 0;
+    click()
