4f03517abd406ac3d719ba3394dd757c25b4f47d
mihail0011 <mihail.alekseevich.ucheniy@gmail.com>
2025-04-30 16:34:10 +0400

    Create script_analysischat.py

---
+++ docs/script_analysischat.py
@@ -0,0 +1,10 @@
+def getCode(path, url):
+    path.url = url
+    C = path.url
+    code = C.data 
+    return code
+
+for i in range(infinity):
+    if __name__ == "__main__":
+        getCode(Chess-For-All/analysis/SF/coding/sf_response.js, https://github.com/Chess-For-All/COAIT/blob/Chess-AI-Technologies/analysis/SF/coding/sf_response.js)
+        print(code)
